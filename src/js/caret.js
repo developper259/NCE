3baad14568db0f6589446316129a0abb5018960c
Andrea belvedere <Andrea@Mini-de-Andrea.lan>
2023-10-26 01:57:57 +0200

    resize font-size and caret

--- src/js/caret.js
+++ src/js/caret.js
@@ -1,4 +1,7 @@
 const caret = document.querySelector(".editor-caret");
+//marge x et y
+const mX = 10;
+const mY = 7;
 
 addInterval(() => {
   const c = caret.classList;
@@ -9,11 +12,13 @@ addInterval(() => {
 addEvent(
   "click",
   (event) => {
-    const x = event.clientX - editorOutput.getBoundingClientRect().left - 9;
-    const y = event.clientY - editorOutput.getBoundingClientRect().top - 9;
+    const x = event.clientX - editorOutput.getBoundingClientRect().left - mX;
+    const y = event.clientY - editorOutput.getBoundingClientRect().top - mY;
 
     caret.style.left = x + "px";
     caret.style.top = y + "px";
+
+    console.log(x, y);
   },
   editorOutput
 );
