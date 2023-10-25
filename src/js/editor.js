479cfa53cd19a52b29e694a2d18583838e4dd456
Andrea belvedere <Andrea@Mini-de-Andrea.lan>
2023-10-25 02:21:40 +0200

    ligne number, caret design,  begin of caret set position when user click and fix bug of the design (css)

--- src/js/editor.js
+++ src/js/editor.js
@@ -4,7 +4,7 @@ let selected = false;
 
 b.addEventListener("click", (e) => {
   const t = e.target;
-  if (t.classList.contains("editor-el")) selected = true;
+  if (t.classList.contains("editor-select")) selected = true;
   else selected = false;
 });
 
@@ -23,3 +23,19 @@ let caretInterval = setInterval(() => {
   if (c.contains("caret-enable") || !selected) c.remove("caret-enable");
   else c.add("caret-enable");
 }, 500);
+
+editorOutput.addEventListener("click", (event) => {
+const x = event.clientX - editorOutput.getBoundingClientRect().left;
+const y = event.clientY - editorOutput.getBoundingClientRect().top;
+
+caret.style.left = x + "px";
+caret.style.top = y + "px";
+
+});
+
+const lineNumbers = document.querySelector(".line-numbers");
+
+lineInterval = setInterval(() => {
+  const lineCount = editorOutput.querySelectorAll("br").length + 1;
+  lineNumbers.innerHTML = Array.from({ length: lineCount }, (_, index) => `<span class="line-el">${index + 1}</span>`).join("");
+}, 100);
