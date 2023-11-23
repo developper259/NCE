3f53e4c195f545b36af237b72a4ddb8adba1faa0
developper259 <trolleur.belvedere@gmail.com>
2023-11-23 23:29:46 +0100

    fix bug with separator

--- src/js/controller/writerController.js
+++ src/js/controller/writerController.js
@@ -1,28 +1,61 @@
 class WriterController {
   constructor(e) {
     this.editor = e;
-    this.separator = [" ", "=", "+", "-", "/", "*"];
+    this.separator = [
+      " ",
+      "!",
+      '"',
+      "#",
+      "$",
+      "%",
+      "&",
+      "'",
+      "(",
+      ")",
+      "*",
+      "+",
+      ",",
+      "-",
+      ".",
+      "/",
+      ":",
+      ";",
+      "<",
+      "=",
+      ">",
+      "?",
+      "@",
+      "[",
+      "]",
+      "^",
+      "_",
+      "`",
+      "{",
+      "|",
+      "}",
+      "~",
+    ];
 
     this.splitWord = (txt) => {
       let oldChar = "";
       let tableSplit = [];
       for (let char of txt) {
-        if (char === '"' && oldChar !== "\\") {
-          tableSplit.push(char);
-          tableSplit.push("");
-        } else {
-          if (this.separator.includes(char)) {
-            tableSplit.push(char);
-            tableSplit.push("");
+        if (this.separator.includes(char)) {
+          if (this.separator.includes(oldChar)) {
+            tableSplit[tableSplit.length - 1] += char;
           } else {
-            if (
-              !tableSplit.length ||
-              typeof tableSplit[tableSplit.length - 1] !== "string"
-            ) {
-              tableSplit.push(char);
-            } else {
-              tableSplit[tableSplit.length - 1] += char;
-            }
+            tableSplit.push("");
+            tableSplit.push(char);
+          }
+        } else {
+          if (
+            !tableSplit.length ||
+            typeof tableSplit[tableSplit.length - 1] !== "string"
+          ) {
+            tableSplit.push(char);
+          } else {
+            if (this.separator.includes(oldChar)) tableSplit.push("");
+            tableSplit[tableSplit.length - 1] += char;
           }
         }
         oldChar = char;
@@ -50,7 +83,7 @@ class WriterController {
             for (let letter of word) {
               wordHTML += `<span class="line-letter editor-select">${letter}</span>`;
             }
-          }else wordHTML += word;
+          } else wordHTML += word;
           let classes = "line-word editor-select";
           if (wordHTML.length == 1) classes += " line-letter";
           resultWords += `<span class="${classes}">${wordHTML}</span>`;
