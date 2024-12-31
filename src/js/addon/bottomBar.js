201270c792be473dbfe8b935b9d1a97db9898176
developper259 <trolleur259@gmail.com>
2024-12-31 16:26:48 +0100

    unstage all function of the constructor in BottomBar file

--- src/js/addon/bottomBar.js
+++ src/js/addon/bottomBar.js
@@ -14,48 +14,46 @@ class BottomBar {
 		this.cursorOBJ = getElement(".bottomBar-cursorPos");
 		this.scrollers = getElements(".scroller-open");
 
-		this.refresh = () => {
-			if (!this.editor.fileManager.activeFile) return;
-
-			let leftBottomBar = getElement('.bottomBar .left');
-			let middleBottomBar = getElement('.bottomBar .middle');
-			let rightBottomBar = getElement('.bottomBar .right');
-			
-			leftBottomBar.style.display = 'flex';
-			middleBottomBar.style.display = 'flex';
-			rightBottomBar.style.display = 'flex';
-
-			this.refreshCursorOBJ();
-
-			for (let scroller of this.scrollers) {
-				let instance = this.scrollersValue[scroller.id].instance;
-				let title = scroller.childNodes[1];
-				title.innerText = instance.values[instance.value];
-				scroller.addEventListener("click", instance.active);
-			}
-		};
-
-		this.refreshCursorOBJ = () => {
-			if (!this.editor.fileManager.activeFile) return;
-			this.editor.selectController.refreshStartEndSelect();
-
-			let r = "";
-			let countLine = this.editor.selectController.getNumberLineSelected();
-
-			if (!countLine) {
-
-				r = `Line ${this.editor.cursor.row}, Column ${this.editor.cursor.column}`;
-			}else{
-				if (countLine > 1) r += countLine + " lines, ";
-				r += this.editor.selectController.containsSelected.length + " characters selected";
-
-			}
-
-			this.cursorOBJ.innerText = r;
-		};
-
 		this.refresh();
 
-		addEvent("onEvent", this.refresh, this.editor.output);
+		addEvent("onEvent", this.refresh.bind(this), this.editor.output);
+	}
+
+	refresh() {
+		if (!this.editor.fileManager.activeFile) return;
+
+		let leftBottomBar = getElement('.bottomBar .left');
+		let middleBottomBar = getElement('.bottomBar .middle');
+		let rightBottomBar = getElement('.bottomBar .right');
+		
+		leftBottomBar.style.display = 'flex';
+		middleBottomBar.style.display = 'flex';
+		rightBottomBar.style.display = 'flex';
+
+		this.refreshCursorOBJ();
+
+		for (let scroller of this.scrollers) {
+			let instance = this.scrollersValue[scroller.id].instance;
+			let title = scroller.childNodes[1];
+			title.innerText = instance.values[instance.value];
+			scroller.addEventListener("click", instance.active);
+		}
+	}
+
+	refreshCursorOBJ() {
+		if (!this.editor.fileManager.activeFile) return;
+		this.editor.selectController.refreshStartEndSelect();
+
+		let r = "";
+		let countLine = this.editor.selectController.getNumberLineSelected();
+
+		if (!countLine) {
+			r = `Line ${this.editor.cursor.row}, Column ${this.editor.cursor.column}`;
+		} else {
+			if (countLine > 1) r += countLine + " lines, ";
+			r += this.editor.selectController.containsSelected.length + " characters selected";
+		}
+
+		this.cursorOBJ.innerText = r;
 	}
 }
