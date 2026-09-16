b2d3294c367442fbb617b8766faf46a22d163e0c
developper259 <trolleur259@gmail.com>
2026-09-16 22:47:50 +0200

    remove dev tools

--- src/ts/Window.ts
+++ src/ts/Window.ts
@@ -160,7 +160,7 @@ export class Window {
       },
     );
 
-    this.window.webContents.toggleDevTools();
+    //this.window.webContents.toggleDevTools();
 
     this.window.webContents.setWindowOpenHandler(({ url }) => {
       if (/^https?:\/\//i.test(url)) shell.openExternal(url);
